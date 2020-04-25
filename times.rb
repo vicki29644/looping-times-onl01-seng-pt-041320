6cf5399927316e3e3f63cc7b86e7738019240924
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-25 09:47:48 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,4 +1,4 @@
-def "using_times"                             puts out phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
+def "using_times" puts out phrase 'Wingardium Leviosa' 7 times by using the `times` construct".
 end 
 
 
